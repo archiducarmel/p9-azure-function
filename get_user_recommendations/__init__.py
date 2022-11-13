3864aa245d1759508c4e10f8060a54e6f66a746e
Sitou AFANOU <51927827+archiducarmel@users.noreply.github.com>
2022-11-13 13:24:09 +0100

    Update __init__.py

--- get_user_recommendations/__init__.py
+++ get_user_recommendations/__init__.py
@@ -4,7 +4,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed a request.Processing ...')
 
     user_id = req.params.get('user_id')
 
